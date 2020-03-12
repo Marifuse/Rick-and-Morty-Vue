3887b3c1a960499aef2f1bdc332b4b33ac1e01f6
María Jesús Fuenzalida Serrano <maria.fuenzalida.serrano@gmail.com>
2020-03-11 21:51:30 -0300

    Primer avance con todo listo y funcional

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template>
+    <div id="main">
+        <!-- Sección que contiene la imagen y el nombre del personaje -->
+        <section class="container">
+            <!-- Función que recorre el arreglo del componente TarjetasPersonajes -->
+            <article class="personajes-list" v-for="(personaje, index) in personajes" :key="index">
+                <TarjetasPersonajes 
+                v-bind:image = "personaje.image"
+                v-bind:nombre = "personaje.name">
+                </TarjetasPersonajes>  
+            </article>             
+        </section>
+    </div>
+</template>
+
+<script>
+// Codigo con el import
+import TarjetasPersonajes from "./TarjetasPersonajes.vue";
+export default {
+    name: "App",
+    components: { TarjetasPersonajes },
+    data() {
+        return {
+             personajes: [
+                {
+                    name: 'Rick Sanchez',         
+                    image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
+                },
+                {
+                    name: 'Morty Smith',         
+                    image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
+                },
+                {
+                    name: 'Summer Smith',         
+                    image: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
+                },
+                {
+                    name: 'Beth Smith',         
+                    image: 'https://rickandmortyapi.com/api/character/avatar/4.jpeg'
+                },
+                {
+                    name: 'Jerry Smith',         
+                    image: 'https://rickandmortyapi.com/api/character/avatar/5.jpeg'
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+    /* Estilo HTML */
+    body {
+        margin: 0;
+        background-image: url(https://rickandmortypod.com/wp-content/uploads/2018/11/cropped-RM_page-header_background1-3.png);
+        background-size: cover;
+    }
+    /* Contenedor imagen y nombre */
+    #main {
+        padding: 5em;
+        padding-top: 2em;
+    }
+</style>
+
---
+++ src/TarjetasPersonajes.vue
@@ -0,0 +1,46 @@
+<template>
+    <div id="main-container">
+        <!-- Contenedor img y personaje -->
+        <section class="container">
+            <div class="p-list">
+                <img v-bind:src="image"/>
+                <h2>{{nombre}}</h2>
+            </div>                
+        </section>
+    </div>
+</template>
+
+<script>
+// Código donde se pone el tipo de nombre e imagen
+export default {
+    name: "TarjetasPersonajes",
+    props: {
+        nombre: String,
+        image: String
+    }
+}
+</script>
+
+<style scoped>
+    /* Contenedor de TarjetasPersonajes */
+    .container {
+        width: 20em;
+        margin: auto;       
+    }
+    .p-list {
+        display: flex;
+        padding: 1em;
+    }
+    /* Imagen contenida en App */
+    img {
+        width: 6em;
+        border-radius: 50%;
+    }
+    /* Nombre contenido en App */
+    h2 {
+        padding-left: 1em;
+        padding-top: 0.5em;
+        color: darkgreen;
+    }
+</style>
+
